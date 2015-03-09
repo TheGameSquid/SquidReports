4df2ebf112828cebcb5fe7b6b2f56cb2b7b194e6
TheGameSquid <robin.mattheussen@gmail.com>
2015-03-09 15:40:30 +0100

    Moved cache functionality

---
+++ SquidReports.DataCollector/API/DbUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using SquidReports.DataCollector.Interface;
+using Dapper;
+
+namespace SquidReports.DataCollector
+{
+    class DbUtils
+    {
+        public static void CleanCache(ICollector collector)
+        {
+            // First things first: spawn a logger
+            NLogManager logManager = new NLogManager();
+            ILogger logger = logManager.GetCurrentClassLogger();
+
+            logger.LogMessage(LogLevel.Info, String.Format("Performing a Cache-sweep for Collector of type {0}", collector.GetType().Name));
+
+            // Get the full list of models that are part of this collector
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            IEnumerable<Int32> IDs = connection.Query<Int32>("SELECT ID FROM [SQR].[DATA_MODEL] WHERE AssemblyName = @AssemblyName", new { AssemblyName = collector.GetType().Assembly.GetName().Name });
+
+            // Handle the Post-Run for all the different models
+            foreach (int ID in IDs)
+            {
+                logger.LogMessage(LogLevel.Debug, String.Format("Performing a Cache-sweep for Model with ID {0}", ID));
+
+                // Step 1: For each model -- Get the full list in the cache, and the full list in the table
+                IEnumerable<String> cachedKeys = connection.Query<String>("SELECT KeyHash FROM [SQR].[DATA_CACHE] WHERE ModelID = @ModelID", new { ModelID = ID });
+                IEnumerable<String> tableKeys = connection.Query<String>("SELECT KeyHash FROM [SQR].[DATA_HASH] WHERE ModelID = @ModelID", new { ModelID = ID });
+
+                // Step 2: For each Key of said model -- Check if the data found in the persistent storage is still valid
+                foreach (string tableKey in tableKeys)
+                {
+                    if (!cachedKeys.Any<String>(ck => ck == tableKey))
+                    {
+                        // Obtain the reference used to link the KeyHash with the ID of the entity in the persistent table
+                        int tableID = connection.Query<Int32>("SELECT TableID FROM [SQR].[DATA_HASH] WHERE ModelID = @ModelID AND KeyHash = @KeyHash", new { ModelID = ID, KeyHash = tableKey }).Single();
+
+                        // Cached keys contain ALL the data that should be retained. The key in the table was NOT found in the cache.
+                        // Therefore, it has become invalid. Let's delete it from the HashTable
+                        connection.Execute("DELETE FROM [SQR].[DATA_HASH] WHERE ModelID = @ModelID AND KeyHash = @KeyHash", new { ModelID = ID, KeyHash = tableKey });
+
+                        // Next up, we need to delete from the actual persistent table, so we need to find out what Model this is
+                        // For that, we use the ModelID and the [SQR].[DATA_MODEL] table
+                        dynamic modelInfo = connection.Query("SELECT * FROM [SQR].[DATA_MODEL] WHERE ID = @ModelID", new { ModelID = ID }).Single();
+                        // Spawn an instance of the model type
+                        object modelObject = Activator.CreateInstance((string)modelInfo.AssemblyName, (string)modelInfo.ModelNameFull).Unwrap();
+                        // Delete based on the Model Type
+                        connection.Execute(Helpers.Sql.DeleteBuilder(modelObject.GetType(), tableID));
+                    }
+                }
+
+                // Step 3: Clear the cache for this model
+                connection.Execute("DELETE FROM [SQR].[DATA_CACHE] WHERE ModelID = @ModelID", new { ModelID = ID });
+            }
+        }
+    }
+}
--- SquidReports.DataCollector/PluginManager/CollectorJobListener.cs
+++ SquidReports.DataCollector/PluginManager/CollectorJobListener.cs
@@ -31,6 +31,28 @@ namespace SquidReports.DataCollector
         public void JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException)
         {
             this.Logger.LogMessage(LogLevel.Info, String.Format("Job {0} completed in {1}", context.JobDetail.Key.Name, context.JobRunTime));
+
+            // We're now entering the post-run phase, so let's perform some checks
+            // Let's unwrap the ICollector object from the JobDataMap
+            ICollector collector = (ICollector)context.MergedJobDataMap["ICollector"];
+
+            // What type of collector is this?
+            CollectorType type;
+            CollectorTypeAttribute[] typeAttributes = (CollectorTypeAttribute[])collector.GetType().GetCustomAttributes(typeof(CollectorTypeAttribute), true);
+            if (typeAttributes.Length > 0)
+            {
+                type = typeAttributes[0].Type;
+            }
+            else
+            {
+                throw new ApplicationException(String.Format("No CollectorType defined for ICollector: {0}", collector.GetType().Name));
+            }
+
+            // An absolute Collector uses a Data Cache to compare data collected during the run with the data already stored in-table
+            if (type == CollectorType.Absolute)
+            {
+                DbUtils.CleanCache(collector);
+            }
         }
     }
 }
